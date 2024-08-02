0f13b78b546a67cfe77915dbcf204c71c7317e41
kinjal0912 <85869248+kinjal0912@users.noreply.github.com>
2024-08-02 18:19:01 +0530

    7.1 commit

--- backend/src/controllers/restaurantControllers/RestaurantController.ts
+++ backend/src/controllers/restaurantControllers/RestaurantController.ts
@@ -1,20 +1,27 @@
-import { Request, Response } from 'express';
-import { Restaurant } from '../../models/restaurant';
+import { Request, Response } from "express";
+import { Restaurant } from "../../models/restaurant";
 
 // Search and filter restaurants
-export const searchRestaurants = async (req: Request, res: Response): Promise<void> => {
+export const searchRestaurants = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
     const { query } = req.query;
     const restaurants = await Restaurant.find({
       $or: [
-        { restaurantName: new RegExp(query as string, 'i') },
-        { city: new RegExp(query as string, 'i') },
-        { country: new RegExp(query as string, 'i') },
-        { cuisines: new RegExp(query as string, 'i') },
+        { restaurantName: new RegExp(query as string, "i") },
+        { city: new RegExp(query as string, "i") },
+        { country: new RegExp(query as string, "i") },
+        { cuisines: new RegExp(query as string, "i") },
       ],
     });
     res.status(200).json(restaurants);
   } catch (error) {
-    res.status(500).json({ error:error.message });
+    if (error instanceof Error) {
+      res.status(500).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: "An unknown error occurred" });
+    }
   }
 };
